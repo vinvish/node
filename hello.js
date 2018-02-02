caa6bb759a9d7afba13f8b5563a868310435baf2
vinvish <36070122+vinvish@users.noreply.github.com>
2018-02-02 20:09:26 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8000, '35.154.134.163');
-console.log('Server running at http://35.154.134.163:8000/');
+}).listen(80, '35.154.134.163');
+console.log('Server running at http://35.154.134.163:80/');
