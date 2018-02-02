af20b5123a13d3a77ffae140f676bdf3b8b678c9
vinvish <36070122+vinvish@users.noreply.github.com>
2018-02-02 22:48:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -86,4 +86,4 @@ socket.on('mobilehandler', function(data){
 
 });
 
-console.log('Your presentation is running on http://0.0.0.0:' + port);
+console.log('Your presentation is running on http://http://13.126.117.107:' + port);
